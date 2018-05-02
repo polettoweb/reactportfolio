30633f12a90d4d692e6609c7ceab5393cec5bebc
Marco <marco@marcopoletto.co.uk>
2018-05-02 08:41:38 +0100

    Remove GA module and added script tag + ignored build folder

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import './App.css';
-import ReactGA from 'react-ga';
-import Header from './components/commons/Header';
-import Footer from './components/commons/Footer';
-import Home from './components/Home';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/commons/Header";
+import Footer from "./components/commons/Footer";
+import Home from "./components/Home";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 // import Contact from './components/Contact';
-import Blog from './components/Blog';
-import Single from './components/Single';
-import Workshop from './components/Workshop';
-ReactGA.initialize('UA-61997681-1');
+import Blog from "./components/Blog";
+import Single from "./components/Single";
+import Workshop from "./components/Workshop";
 class App extends Component {
-    fireTracking() {
-        ReactGA.pageview(window.location.pathname + window.location.search)
-    }
-    render() {
-        return (
-            <div className="App">
-                <Router onUpdate={this.fireTracking}>
-                    <div>
-                        <Header />
-                        <Route exact path="/" component={Home} />
-                        <Route path="/portfolio" component={Portfolio}/>
-                        <Route path="/resume" component={Resume} />
-                        <Route exact path="/Blog" component={Blog} />
-                        <Route path="/Blog/:slug/:id" component={Single} />
-                        <Route path="/workshop" component={Workshop} />
-                        <Footer />
-                    </div>
-                </Router>
-            </div>
-        );
-    }
+   render() {
+      return (
+         <div className="App">
+            <Router>
+               <div>
+                  <Header />
+                  <Route exact path="/" component={Home} />
+                  <Route path="/portfolio" component={Portfolio} />
+                  <Route path="/resume" component={Resume} />
+                  <Route exact path="/Blog" component={Blog} />
+                  <Route path="/Blog/:slug/:id" component={Single} />
+                  <Route path="/workshop" component={Workshop} />
+                  <Footer />
+               </div>
+            </Router>
+         </div>
+      );
+   }
 }
 
 export default App;
